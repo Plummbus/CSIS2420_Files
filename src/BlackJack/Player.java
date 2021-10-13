f23576cab21b374c19f55e4d184eda2290b0b57e
Franz <franzjacobh@gmail.com>
2021-10-13 09:51:00 -0600

    Main Menu added
    Menu menu works, added some new fields to Player.java, and you can now bet money

--- src/BlackJack/Player.java
+++ src/BlackJack/Player.java
@@ -4,14 +4,22 @@ public class Player {
 
 	private String name;
 	private int money;
+	private int currentBet;
 	private int total;
+	private boolean wonBet;
 	private Card[] hand;
+	private int wins;
+	private int losses;
 	
-	public Player (String name, int total) {
+	public Player (String name) {
 		this.name = name;
 		this.money = 500;
+		this.currentBet = 0;
 		this.total = 0;
+		this.wonBet = false;
 		this.hand = new Card[2];
+		this.wins = 0;
+		this.losses = 0;
 	}
 	
 	public String getName() {
@@ -22,12 +30,44 @@ public class Player {
 		return this.money;
 	}
 	
+	public int getCurrentBet() {
+		return this.currentBet;
+	}
+	
 	public int getTotal() {
 		return this.total;
 	}
 	
+	public boolean hasWonBet() {
+		return this.wonBet;
+	}
+	
 	public Card[] getHand() {
 		return this.hand;
+	}
+	
+	public int getWins() {
+		return this.wins;
+	}
+	
+	public int getLosses() {
+		return this.losses;
+	}
+	
+	public void setCurrentBet(int bet) {
+		this.currentBet = bet;
+	}
+	
+	public void setWonBet(boolean value) {
+		this.wonBet = value;
+	}
+	
+	public void win() {
+		this.wins++;
+	}
+	
+	public void lose() {
+		this.losses++;
 	}
 	
 	public void printHand() {
@@ -43,6 +83,14 @@ public class Player {
 			hand[0] = card;
 		} else {
 			hand[1] = card;
+		}
+	}
+	
+	public void updateTotal() {
+		if (hasWonBet()) {
+			this.money += Math.round(getCurrentBet() * 1.5f);
+		} else {
+			this.money -= getCurrentBet();
 		}
 	}
 }
