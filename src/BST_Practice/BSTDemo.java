826ffb89444b3f59863e928eb28f548afd0bb1d4
Franz <franzjacobh@gmail.com>
2021-10-24 15:10:48 -0600

    End-of-day
    Basic traversals in BSTs, need to learn how to search/delete.

--- src/BST_Practice/BSTDemo.java
+++ src/BST_Practice/BSTDemo.java
@@ -4,6 +4,21 @@ public class BSTDemo {
 	
 	public static void main(String[] args) {
 		
+		BinarySearchTree bst = new BinarySearchTree(new BSTNode(10));
+		bst.insert(3);
+		bst.insert(4);
+		bst.insert(5);
+		bst.insert(12);
+		bst.insert(11);
+		
+		bst.preOrderTraverse(bst.getRoot());
+		System.out.println();
+		
+		bst.inOrderTraverse(bst.getRoot());
+		System.out.println();
+		
+		bst.postOrderTraverse(bst.getRoot());
+		
 	}
 
 }
--- src/BST_Practice/BinarySearchTree.java
+++ src/BST_Practice/BinarySearchTree.java
@@ -9,17 +9,21 @@ public class BinarySearchTree {
 		this.root = root;
 	}
 	
+	public BSTNode getRoot() {
+		return this.root;
+	}
+	
 	public void insert(int data) {
 		BSTNode newNode = new BSTNode(data);
 		if (root == null) {
 			this.root = newNode;
 		} else {
-			BSTNode current = newNode;
+			BSTNode current = root;
 			BSTNode parent = null;
 			boolean insertComplete = false;
 			while (!insertComplete) {
 				parent = current;
-				if (newNode.getData() < root.getData()) {
+				if (newNode.getData() < current.getData()) {
 					current = current.getLeft();
 					if (current == null) {
 						parent.setLeft(newNode);
@@ -34,5 +38,61 @@ public class BinarySearchTree {
 				}
 			}
 		}
+	}
+	
+	//prints contents of tree in order of: root, left side, then right side
+	//must pass the root as the parameter on first method call.
+	//method takes a parameter like this for recursion.
+	public void preOrderTraverse(BSTNode node) {
+		//base case?
+		if (root == null) {
+			System.out.println("Tree is empty.");
+			return;	//just want to make sure we stop function at this point;
+		} else {
+			System.out.println(node.getData() + " ");
+			if (node.getLeft() != null) {
+				preOrderTraverse(node.getLeft());
+			}
+			if (node.getRight() != null) {
+				preOrderTraverse(node.getRight());
+			}
+		}
+	}
+	
+	//same concept as preOrderTraverse(node)
+	//this time it prints: left side, root, right side
+	public void inOrderTraverse(BSTNode node) {
+		if (root == null) {
+			System.out.println("Tree is empty.");
+			return;
+		} else {
+			if (node.getLeft() != null) {
+				inOrderTraverse(node.getLeft());
+			}
+			System.out.println(node.getData() + " ");
+			if (node.getRight() != null) {
+				inOrderTraverse(node.getRight());
+			}
+		}
+	}
+	
+	//same concept as preOrderTraverse(node)
+	//this time it prints: right side, root, left side
+	public void postOrderTraverse(BSTNode node) {
+		if (root == null) {
+			System.out.println("Tree is empty.");
+		} else {
+			if (node.getRight() != null) {
+				postOrderTraverse(node.getRight());
+			}
+			System.out.println(node.getData() + " ");
+			if (node.getLeft() != null) {
+				postOrderTraverse(node.getLeft());
+			}
+		}
+	}
+	
+	public BSTNode search(int data) {
+		return null;
 	}
 }
